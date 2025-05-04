b3c0e701194ed0ea6fde2ea738ca616cf9d36fd7
Antrax <fredcox@gmail.com>
2025-05-04 07:53:38 -0300

    add click.py and constants. using click

---
+++ src/hyprtiler/click.py
@@ -0,0 +1,42 @@
+from .constants import APP_NAME, APP_VERSION
+from hyprtiler.config import writeConfigFile
+import click
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+class CustomHelpCommand(click.Command):
+    def format_help(self, ctx: click.Context, formatter: click.HelpFormatter) -> None:
+        """Custom help formatter that includes app name and version."""
+        formatter.write(f"{APP_NAME} v{APP_VERSION}\n\n")
+        super().format_help(ctx=ctx, formatter=formatter)
+
+
+@click.option(
+    "-r",
+    "--rule",
+    "rule",
+    type=click.STRING,
+    default="float",
+    help="Specifies the Rule for window. (default: float)",
+)
+@click.option(
+    "-c",
+    "--class",
+    "window_class",
+    type=click.STRING,
+    required=True,
+    help="Windows Class",
+)
+@click.command(cls=CustomHelpCommand, context_settings=CONTEXT_SETTINGS)
+def cli(rule, window_class) -> None:
+    """A utility tool for managing windows in the Hyprland compositor environment."""
+    click.echo(message=f"{APP_NAME} v{APP_VERSION}\n")
+
+    if rule:
+        click.echo(f"Rule: {rule} ")
+
+    if window_class:
+        click.echo(f"Window Class: {window_class:} ")
+
+    writeConfigFile(rule, window_class)
