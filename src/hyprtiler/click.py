6c2616df7fa5dccc8998fb33cf3aafd189f2dd06
Antrax <fredcox@gmail.com>
2025-05-04 08:18:10 -0300

    Release 0.1.20

--- src/hyprtiler/click.py
+++ src/hyprtiler/click.py
@@ -25,23 +25,20 @@ class CustomHelpCommand(click.Command):
     "--class",
     "window_class",
     type=click.STRING,
-    # required=True,
     help="Windows Class",
 )
 @click.command(cls=CustomHelpCommand, context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 def cli(ctx, rule, window_class) -> None:
     """A utility tool for managing windows in the Hyprland compositor environment."""
-    click.echo(message=f"{APP_NAME} v{APP_VERSION}\n")
-
     if not window_class:
-        click.echo("Error: --class is required.")
-
+        ctx.obj = {}  # Ensure ctx.obj exists
+        click.echo(ctx.get_help())
         ctx.exit(1)
 
-    if rule:
-        click.echo(f"Rule: {rule}")
+    click.echo(message=f"{APP_NAME} v{APP_VERSION}\n")
 
+    click.echo(f"Rule: {rule}")
     click.echo(f"Window Class: {window_class}")
 
     writeConfigFile(rule, window_class)
